69f18ae5e50828442ca34630465c036137864c48
nikolemc <nikolemcstanley@gmail.com>
2017-04-06 15:00:49 -0400

    Create Library Database in SQL. API is able to get a list of all books with there status and data

---
+++ LibraryAPI/Controllers/LibraryController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LibraryAPI.Models;
+
+namespace LibraryAPI.Controllers
+{
+    public class LibraryController : ApiController
+    {
+        const string connectionString =
+               @"Server=localhost\SQLEXPRESS;Database=LibraryData;Trusted_Connection=True;";
+
+        [HttpGet]
+        public IHttpActionResult GetAllBooks()
+        {
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+
+                var books = new List<LibraryCatalog>();
+                var sqlCommand = new SqlCommand(@"SELECT [Id],[Title],[Author],[YearPublished],[Genre],[IsCheckedOut],[LastCheckedOutDate],[DueBackDate]
+                                            FROM [dbo].[LibraryCatalog]", connection);
+                connection.Open();
+                var reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    //var course = new Course(reader);
+                    var book = new LibraryCatalog
+                    {
+                    Id = (int)reader["Id"],
+                    Title = reader["Title"].ToString(),
+                    YearPublished = (int)reader["YearPublished"],
+                    Genre = reader["Genre"].ToString(),
+                    IsCheckedOut = (bool)reader["IsCheckedOut"],
+                    LastCheckedOutDate = reader["LastCheckedOutDate"] as DateTime?,
+                    DueBackDate = reader["DueBackDate"] as DateTime?,
+                };
+                books.Add(book);
+            }
+            // Close Connection
+            connection.Close();
+            return Ok(books);
+        }
+
+    }
+
+
+
+
+
+
+
+}
+}
---
+++ LibraryAPI/Models/LibraryCatalog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace LibraryAPI.Models
+{
+    public class LibraryCatalog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int YearPublished { get; set; }
+        public string Genre { get; set; }
+        public bool IsCheckedOut { get; set; }
+        public DateTime? LastCheckedOutDate { get; set; }
+        public DateTime? DueBackDate { get; set; }
+
+        public LibraryCatalog()
+        {
+
+        }
+
+        public LibraryCatalog(SqlDataReader reader)
+        {
+            Id = (int)reader["Id"];
+            Title = reader["Title"].ToString();
+            YearPublished = (int)reader["YearPublished"];
+            Genre = reader["Genre"].ToString();
+            IsCheckedOut = (bool)reader["IsCheckedOut"];
+            LastCheckedOutDate = (DateTime)reader["LastCheckedOutDate"];
+            DueBackDate = (DateTime)reader["DueBackDate"];
+
+        }
+    }
+}
